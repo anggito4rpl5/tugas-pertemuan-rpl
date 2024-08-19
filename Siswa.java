e48e871fb96eca8dcb85b8c4e96cedbeaf032b99
anggito4rpl5 <anggito_abimanyu_33rpl@student.smktelkom-mlg.sch.id>
2024-08-19 14:18:24 +0700

    first commit

---
+++ Siswa.java
@@ -0,0 +1,24 @@
+public class Siswa {
+    // variabel
+    int id;
+    String nama;
+    double ipk;
+
+    public Siswa() {
+
+        id = 0;
+        nama = "Kosong";
+        ipk = 0.0;
+
+    }
+
+    // Getter
+    // Setter
+    // Method
+public void print () {
+    System.out.println("ID : " + id );
+    System.out.println("Nama : " + nama );
+    System.out.println("IPK : " + ipk );
+
+}
+}
---
+++ SiswaTester.java
@@ -0,0 +1,42 @@
+public class SiswaTester {
+    public static void main(String[] args) {
+   //membuat object
+   //namaClass namaObject = new
+Siswa Anggito = new Siswa ();
+Siswa Dewangga = new Siswa ();
+Siswa Nafis = new Siswa ();
+Siswa Amin = new Siswa ();
+Siswa Rehan = new Siswa ();
+
+    
+    Anggito.nama = "Anggito Abimanyu";
+    Anggito.id = 4;
+    Anggito.ipk = 70.0;
+
+    Dewangga.nama = "Dewangga Firmansyah ";
+    Dewangga.id = 9;
+    Dewangga.ipk = 96.7;
+
+    Nafis.nama = "Nafis Dwi Harits";
+    Nafis.id = 25;
+    Nafis.ipk = 120.5;
+
+    Amin.nama = "Muhammad Amin";
+    Amin.id = 21;
+    Amin.ipk = 90.6;
+
+    Rehan.nama = "Rehan Ardhanis";
+    Rehan.id = 29;
+    Rehan.ipk = 62.4;
+
+Anggito.print();
+Dewangga.print();
+Nafis.print();
+Amin.print();
+Rehan.print();
+
+
+
+
+    }
+}
